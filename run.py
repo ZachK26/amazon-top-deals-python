83a6903480889e37674bf513106e5dd1d71fbb6a
ZachK26 <63168062+ZachK26@users.noreply.github.com>
2021-04-29 16:44:04 -0500

    Bot and test layout
    now sends messages into discord and has a test layout for information gathering

--- run.py
+++ run.py
@@ -1,14 +1,35 @@
 from Crawler import Crawler
 import webbrowser
+import discord
+import time
 
-store = str(input("Enter the desired amazon extension (com, it, fr, co.uk ...): "))
+bot = discord.Client()
 
-product_name = str(input("Enter the name of the product you are looking for: "))
-print("NOTE: Do not use your internet while running this script. It may cause the script to halt prematurely...")
-print("NOTE: The top 5 best deals will open in your default web browser automatically once the script finishes executing")
-print("<< Ignore any index related errors >>")
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
 
-craw = Crawler(product_name, store)
-top_five = craw.get_best_deal()
-for item in top_five:
-    webbrowser.open_new_tab(item["link"])
+store = "com"
+
+product_name = "Gaming Mouse"
+
+
+@bot.event
+async def on_message(message):
+
+    if message.content.startswith('getdeals'):
+        await message.channel.send("Getting deals... Give me a moment...")
+        craw = Crawler(product_name, store)
+        top_ten = craw.get_best_deal()
+        await message.channel.send("Here are the best deals!")
+        for item in top_ten:
+            phrase = item["link"]
+            name = item["name"]
+            discount = str(item["price_diff"])
+            await message.channel.send(name + " at a $" + discount + " discount: " + phrase)  
+            time.sleep(6)
+        time.sleep(3600)
+        await message.channel.send("getdeals")
+    if message.content.startswith('getdeals'):
+        await message.delete(message) 
+bot.run('ODM3NDIxOTY3NDIxMjEwNjQ2.YIsT-w.9cS9a3nKACYvMir75S-2DuegQ_E')
